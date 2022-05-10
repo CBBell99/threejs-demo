03a0b38d0672f67c5ba70bf4530a1d4a496aff1c
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-09 22:53:26 -0700

    display torus

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+renderer.render(scene, camera)
